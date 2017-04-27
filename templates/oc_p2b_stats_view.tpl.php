4efb6078bf6c13173d700c7fb8a909dc3bde255e
Tobias F. Petersen <tobsf@hotmail.com>
2017-04-27 15:03:59 +0200

    colums everywhere

--- templates/oc_p2b_stats_view.tpl.php
+++ templates/oc_p2b_stats_view.tpl.php
@@ -2,7 +2,7 @@
     
     <div class="align-center" style="">
     <?php
-    $pie_data  = isset($pie_data) ? $pie_data : $bar_data;
+    $pie_data  = isset($pie_data) && $pie_data != null ? $pie_data : $bar_data;
     foreach ($pie_data as $key => $value) { ?>
     
         <div id="<?php echo $key . "_container" ?>" style="min-width: 100%; height: 400px; max-width: 100%; margin: 0 auto"></div>
